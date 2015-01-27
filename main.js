3fe70e3bde26defcf7251b3472fdde25cec7caeb
Po Linn Chia <chiapolinn@gmail.com>
2015-01-27 14:26:23 -0700

    Refactor a lot of code into functions

--- main.js
+++ main.js
@@ -1,36 +1,71 @@
 $(document).on('ready', function() {
 
-  var month = [];
-  month[0] = "January";
-  month[1] = "February";
-  month[2] = "March";
-  month[3] = "April";
-  month[4] = "May";
-  month[5] = "June";
-  month[6] = "July";
-  month[7] = "August";
-  month[8] = "September";
-  month[9] = "October";
-  month[10] = "November";
-  month[11] = "December";
 
-  var now = new Date();
 
-  var thisMonth = $('<div class="month-wrapper"></div>');
-  thisMonth.append('<p class="month">' + month[now.getMonth()] + '</p>');
 
-  var thisWeek = $('<p>Today</p>')
-      .append('<p>Tomorrow</p>');
 
-  var numericalDays = [ now.getDate()+2, now.getDate()+3, now.getDate()+4 ];
-  var numericalDaysEls = numericalDays.map(function(days){
-    return $('<p>' + days + '</p>');
-  });
+  // SET UP FIRST WEEK //////////////////////////////////////
 
-  thisWeek.append(numericalDaysEls);
-  thisMonth.append(thisWeek);
+  var now = moment();
+  var thisMonth = now.format("MMMM");
 
-  $(".calendar").append(thisMonth);
+  var wrapper = $('.calendar');
+  wrapper.append('<p class="month">' + thisMonth + '</p>');
+
+  // Helper function for creating an array of the next 7 days
+  var nextWeek = function(){
+    var newWeek = [ now.add(1,'d').format("D"), 
+                        now.add(1,'d').format("D"), 
+                        now.add(1,'d').format("D"),
+                        now.add(1,'d').format("D"), 
+                        now.add(1,'d').format("D"), 
+                        now.add(1,'d').format("D"),
+                        now.add(1,'d').format("D")   
+                  ];
+    return newWeek;
+  };
+
+  // Helper function for splicing in textual months to a list of 
+  // running numerical days
+  var spliceInMonths = function(days){
+    for (var i=0; i<days.length; i++){
+      var j = i + 1;
+      if (days[j] === undefined){
+        break;
+      }
+      if (days[j] < days[i]){
+        days.splice(j,0, now.format("MMMM"));
+        i++;
+      }
+    }
+    return days;
+  };
+
+  // Helper function for injecting HTML into day/month formatting
+  var formatMonth = function(day){
+    var reg = /^\d+$/;
+    if (reg.test(day))
+      return $('<p>' + day + '</p>');
+    else 
+      return $('<p class="month">' + day + '</p>');
+  };                    
+  
+
+  wrapper.append('<p>Today</p>');
+  var numericalDays = nextWeek();
+  numericalDays = spliceInMonths(numericalDays).map(formatMonth);
+  wrapper.append(numericalDays);
+
+  $(".calendar").append(monthWrapper);
+
+
+  // PAGINATE, YO /////////////////////////////////////////////////////////////
+
+  $(window).scroll(function(){
+    if  ($(window).scrollTop() == $(document).height() - $(window).height()){
+          // run our call for pagination
+    }
+  }); 
 
   
 });
